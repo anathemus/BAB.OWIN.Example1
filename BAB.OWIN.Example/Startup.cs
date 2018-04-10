e5ad37b36aa5bcb2011b91dd09bac2130bfd684e
BenjaminABurgess <anathemus@gmail.com>
2018-04-10 10:41:30 -0400

    * BAB.OWIN.Example.csproj: Created simple OWIN Pipeline, "Hello   World" to browser
    * Startup.cs:
    * Web.config:
    * packages.config:

---
+++ BAB.OWIN.Example/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Owin;
+using System.Diagnostics;
+
+namespace BAB.OWIN.Example
+{
+    public class Startup
+    {
+       
+            public static void Configuration(IAppBuilder app)
+            {
+            app.Use(async (ctx, next) => {
+                Debug.WriteLine("Incoming Request: " + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outgoing Request: " + ctx.Request.Path);
+            });
+
+                app.Use(async (ctx, next) => {
+                    await ctx.Response.WriteAsync("<html><head></head><body>Hello World</body></html>");
+                });
+            }
+
+    }
+}
